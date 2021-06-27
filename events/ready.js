560cfb848f6709b48c56d01f9224b460b70090d3
Will <80347843+willlupus@users.noreply.github.com>
2021-06-27 19:16:07 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const moment = require('moment');
-const chalk = require('chalk');
-const {prefix} = require('../config.json');
-
-module.exports = client => (
-    console.log(`Bot Hazır ${client.user.tag} \nEmrinize Amade \nHer şeyi en doğru şekilde yükledim `)
-)
