268b58926f7ba4e609ae78472abca62ef1046bac
Will <80347843+willlupus@users.noreply.github.com>
2021-06-27 19:16:14 +0300

    Delete util directory

--- util/eventHandler.js
+++
@@ -1,5 +0,0 @@
-const reqEvet = (event) => require(`../events/${event}`)
-module.exports = client => {
-    client.on("ready" , () => reqEvet("ready.js") (client, "message") );
-        client.on("message",reqEvet("message"));
-}
