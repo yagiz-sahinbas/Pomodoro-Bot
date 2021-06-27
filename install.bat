253cfcba4519f116939e9dfff5e9f3eb823c22c9
Will <80347843+willlupus@users.noreply.github.com>
2021-06-27 19:40:32 +0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,10 @@
+npm install ascii-table
+npm install chalk
+npm install easy-json-database
+npm install ffmpeg-static
+npm install moment
+npm install ms
+npm install nodemon
+npm install opusscript
+npm test
+
